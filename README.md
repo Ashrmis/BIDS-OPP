6897f3182306038c05c5cd0aec2bb27e30be2194
Ashish Mistry <ashishmistry19@yahoo.com>
2021-01-30 16:24:16 -0500

    Readme + sample python file

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learning git + Python object oriented programming @ the same time
---
+++ test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 16:21:25 2021
+
+@author: Ashish
+"""
+
+disp("Hello World")
