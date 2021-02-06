0b6a73d17deebe9eec2334e4c24564ee0d581b12
ashish <ashishmistry19@yahoo.com>
2021-02-05 21:07:13 -0500

    light edits, next time figure out session with class

--- Participant_class.py
+++ Participant_class.py
@@ -4,34 +4,31 @@ Participant Class
 - Group; ms, hc etc
 - ID number, 001, 002
 - Sessions, can give an integer
-
-
---- Later ---
-- auto lowercase
-- format the ID number under sub-hc000
-- adding additional session
-- Age
-- Gender
--------------
 """
 
 class Participant:
-    def __init__(self,group,ID,session):
+    def __init__(self,group,ID,session,parent,addsession):
         self.group=group
         self.ID=ID
         self.session=session
-        
+        self.parent=parent
         self.subname='sub-' + group + ID
         
         return
     
-    """
-    Functions Needed:
-        - Create the study directory if it doesn't exist
-        - Create the subject directory if it doesn't exist
-            (will handle adding sessions later on)
     
     
-    """
+    # Create a function that makes session folders/paths ready:
+    #       - parent/ses-[session-label]/{anat,func,fmap,dwi}
+    #
+    #   ^^^^---- This then gets called from the run_file as:
+    #            sub=Participant(group,id,session)
+    #            sub.create(study)
 
 
+
+    #def createpaths(self,parent,session,addsession):
+
+        #return subpaths
+    
+   
--- create_BIDS_filestruct.py
+++ create_BIDS_filestruct.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python
-
 from Participant_class import Participant
 import sys
-
 import getopt
+import os
 
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'g:i:s', ['group=', 'id=', 'sessions='])
+    opts, args = getopt.getopt(sys.argv[1:], 'g:i:s:d:a', ['group=', 'id=', 'sessions=', 'studydir=','addsession='])
 except getopt.GetoptError:
     sys.exit(2)
-
-
-
-
+    
+# set inputs
 for opt, arg in opts:
     if opt in ('-g', '--group'):
         group=arg
@@ -21,8 +18,24 @@ for opt, arg in opts:
         id = arg
     elif opt in ('-s', '--sessions'):
         ses_cnt = arg
-
+    elif opt in ('-d', '--studydir'):
+        study = arg
+    elif opt in ('-a', '--addsession'):
+        new_session = arg
 
 sub1 = Participant(group,id,ses_cnt)
 
+#   - directory exists if it doesn't exist then create it
+if os.path.isdir(study) is False:
+    print 'Created' ,study, 'as the parent study directory'
+    os.makedirs(study)
+    
+
 print(sub1.subname)
+print(study)
+print(new_session)
+
+# Checks
+
+#   - new session should be of ses-0X format
+#   - ses_cnt should be nonzero, > 0, integer
